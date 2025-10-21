3412066b4b78ef09431096990f2e95ca4eaaf809
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-21 10:02:56 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,16 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('dolar-app-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+    caches.open('dolar-app-cache').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
   );
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
